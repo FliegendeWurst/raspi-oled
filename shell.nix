d8f97437e940389a98030fb53e9ba1b42ab2387e
FliegendeWurst <2012gdwu+github@posteo.de>
2021-10-09 12:06:37 +0200

    Unbundle sqlite3 and link it instead
    To run the cross compiled binary on Debian, do
     patchelf --set-interpreter /lib/ld-linux-armhf.so.3 raspi-oled

--- shell.nix
+++ shell.nix
@@ -2,6 +2,8 @@ with (import <nixpkgs> {});
 mkShell {
   nativeBuildInputs = [
     pkgsCross.muslpi.stdenv.cc
+    pkgsCross.muslpi.sqlite
   ];
   CARGO_TARGET_ARM_UNKNOWN_LINUX_MUSLEABIHF_LINKER = "${pkgsCross.muslpi.stdenv.cc}/bin/armv6l-unknown-linux-musleabihf-ld";
+  #TARGET_CC = "${pkgsCross.muslpi.stdenv.cc}/bin/armv6l-unknown-linux-musleabihf-cc";
 }
